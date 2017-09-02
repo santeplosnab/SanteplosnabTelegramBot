5d186e39f7e275d0ecfeaa43b7cc0bbd25781b47
Дмитрий Щербаков <eshxe@mail.ru>
2017-09-02 16:16:15 +0400

    основной процесс

--- SanteplosnabTelegramBot/MainForm.Designer.cs
+++ SanteplosnabTelegramBot/MainForm.Designer.cs
@@ -28,20 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBoxApiKey = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonConnect = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // textBoxApiKey
+            // 
+            this.textBoxApiKey.Location = new System.Drawing.Point(92, 12);
+            this.textBoxApiKey.Name = "textBoxApiKey";
+            this.textBoxApiKey.Size = new System.Drawing.Size(452, 20);
+            this.textBoxApiKey.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "API-key";
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(365, 46);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(178, 22);
+            this.buttonConnect.TabIndex = 2;
+            this.buttonConnect.Text = "Включить бота";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(556, 132);
+            this.ClientSize = new System.Drawing.Size(556, 82);
+            this.Controls.Add(this.buttonConnect);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxApiKey);
             this.Name = "MainForm";
             this.Text = "Панель управления ботом Telegram";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBoxApiKey;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonConnect;
     }
 }
 
--- SanteplosnabTelegramBot/MainForm.cs
+++ SanteplosnabTelegramBot/MainForm.cs
@@ -12,9 +12,80 @@ namespace SanteplosnabTelegramBot
 {
     public partial class MainForm : Form
     {
+        BackgroundWorker bw;
+
         public MainForm()
         {
             InitializeComponent();
+
+            this.bw = new BackgroundWorker();
+
+            this.bw.DoWork += this.bw_DoWork;
+        }
+
+        /// <summary>
+        /// Основной процесс бота
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void bw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            var worker = sender as BackgroundWorker;
+            var key = e.Argument as string;
+
+            try
+            {
+                var Bot = new Telegram.Bot.TelegramBotClient(key);
+
+                await Bot.SetWebhookAsync("");
+
+                int offset = 0;
+
+                // Основной цикл программы
+                while (true)
+                {
+                    var updates = await Bot.GetUpdatesAsync(offset);
+
+                    foreach(var update in updates)
+                    {
+                        var message = update.Message;
+
+                        Console.WriteLine(update.Type);
+
+                        if(message.Type == Telegram.Bot.Types.Enums.MessageType.TextMessage)
+                        {
+                            if (message.Text.Equals("/тест"))
+                            {
+                                await Bot.SendTextMessageAsync(message.Chat.Id, "Привет, это бот компании СанТеплоСнаб. В данный момент меня еще пилят " +
+                                    "и я ничего не умею :(", replyToMessageId: message.MessageId);
+                            }
+
+                            if (message.Text.Equals("/счет"))
+                            {
+                                await Bot.SendTextMessageAsync(message.Chat.Id, "От какой организации выставить счет?",
+                                    replyToMessageId: message.MessageId);
+                            }
+                        }
+                        offset += update.Id + 1;
+                    }
+                }
+
+
+            }
+            catch (Telegram.Bot.Exceptions.ApiRequestException ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            var text = textBoxApiKey.Text;
+
+            if (!this.bw.IsBusy)
+            {
+                this.bw.RunWorkerAsync(text);
+            }
         }
     }
 }
